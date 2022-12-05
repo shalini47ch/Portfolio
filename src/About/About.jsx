01803ff627df824278a9e77d22205cc668225ebf
shalini47ch <shalini47choudhary@gmail.com>
2022-12-05 19:32:56 +0530

    skills and about updated

--- src/About/About.jsx
+++ src/About/About.jsx
@@ -1,31 +1,29 @@
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import logo from "./shalini.jpg";
-import {FcGraduationCap} from "react-icons/fc"
+import { FcGraduationCap } from "react-icons/fc";
 import "./style.css";
 
 const About = () => {
   return (
     <div className="about">
-      <img src={logo} alt="icon"/>
+      <img src={logo} alt="icon" />
       <p className="user-info">
-      <FcGraduationCap size="60px"/>
+        <FcGraduationCap size="60px" />
         <h1>
           <span>{"<"}</span>Hello World!<span>{"/>"} </span>
         </h1>
         <br />
         <div className="data">
-          I am Shalini Choudhary,a Frontend Developer with hands on experience
-          in HTML,CSS,JS,ReactJS.Build a lot of reusable components using
-          ReactJs.Apart from frontend I am also skilled in data structures and
-          algorithms. Keen interest areas include Development and Analytics
-          ,have hands-on experience in python along with data visualization
-          tools.Apart from technical knowledge I also like reading novels and
-          painting.
+          <p>
+            Shalini has a lot of expertise with React JS for building front-end
+            applications while adhering to the best coding principles. She has
+            experience utilizing HTML, CSS, and JavaScript to create responsive
+            and accessible web pages. She has a strong understanding of data
+            structures and algorithms and can solve problems quickly.
+          </p>
         </div>
-      
       </p>
-     
     </div>
   );
 };
--- src/Skills/Skills.jsx
+++ src/Skills/Skills.jsx
@@ -1,40 +1,92 @@
 import React from "react";
 import "./style.css";
-import Button from "./Button";
-import WebTech from "./WebTech";
-import Deploy from "./Deploy";
-import Library from "./Library"
-import Tools from './Tools'
+import { DiJavascript, DiPython } from "react-icons/di";
+import {
+  SiCplusplus,
+  SiHtml5,
+  SiCss3,
+  SiTensorflow,
+  SiKeras,
+  SiVisualstudiocode,
+  SiSublimetext,
+  SiBitbucket,
+  SiPostman,
+  SiLeetcode,
+  SiGeeksforgeeks,
+} from "react-icons/si";
+import { BsBootstrapFill } from "react-icons/bs";
+import { ImAccessibility } from "react-icons/im";
+import { GrReactjs } from "react-icons/gr";
+import { AiOutlineGithub } from "react-icons/ai";
+import { RiGitlabFill } from "react-icons/ri";
+import { FaHackerrank } from "react-icons/fa";
+
+const leetcode = () => {
+  window.location.href = "https://leetcode.com/shalini47choudhary/";
+};
+
+const gfg = () => {
+  window.location.href =
+    "https://auth.geeksforgeeks.org/user/shalinichoudhary/practice/";
+};
+
+const hackerrank = () => {
+  window.location.href = "https://www.hackerrank.com/Shalini09";
+};
 
 const Skills = () => {
   return (
     <>
-      <div className="skills">
-        <div className="programming-languages">
-         <span>Languages</span>
-       <Button/>
-       </div>
-       <div className="web-tech">
-       <span>Web Technologies</span>
-       <WebTech/>
-       </div>
-       <div className="deploy">
-       <span>Deployment</span>
-       <Deploy/>
-       </div>
-       <div className="library">
-         <span>Libraries and Frameworks</span>
-       <Library/>
-       </div>
-       <div className="tools">
-       <span>Tools</span>
-       <Tools/>
-       </div>
+      <div>
+        <div className="languages">
+          <p>Languages</p>
+          <div>
+            <DiJavascript color="yellow" size="50px" />
 
+            <DiPython color="blue" size="50px" />
 
-         
-         
+            <SiCplusplus color="red" size="50px" />
+          </div>
         </div>
+
+        <div className="web-technolgies">
+          <p>Web Technologies</p>
+          <div className="icons2">
+            <SiHtml5 color="orange" size="50px" />
+            <SiCss3 color="yellow" size="50px" />
+            <BsBootstrapFill color="purple" size="50px" />
+            <ImAccessibility color="white" size="50px" />
+          </div>
+        </div>
+
+        <div className="libraries">
+          <p>Libraries and Framework</p>
+          <div className="icons-3">
+            <GrReactjs color="blue" size="50px" />
+            <SiTensorflow color="orange" size="50px" />
+            <SiKeras color="red" size="50px" />
+          </div>
+        </div>
+        <div className="tools">
+          <p>Tools</p>
+          <div className="icons-4">
+            <SiVisualstudiocode color="blue" size="50px" />
+            <SiSublimetext color="orange" size="50px" />
+            <AiOutlineGithub color="white" size="50px" />
+            <RiGitlabFill color="orange" size="50px" />
+            <SiBitbucket color="blue" size="50px" />
+            <SiPostman color="orange" size="50px" />
+          </div>
+        </div>
+      </div>
+      <div className="problem-solving">
+        <p>Check coding profiles</p>
+        <div className="icons-5">
+          <SiLeetcode color="orange" size="50px" onClick={leetcode} />
+          <SiGeeksforgeeks color="green" size="50px" onClick={gfg} />
+          <FaHackerrank color="green" size="50px" onClick={hackerrank} />
+        </div>
+      </div>
     </>
   );
 };
